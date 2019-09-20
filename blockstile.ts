d5dbe8952e2052bedb912fc5eeba3f4a9768fe6e
Tom Ball <tballmsft@users.noreply.github.com>
2019-09-20 14:28:56 -0700

    a start on blocks

--- blockstile.ts
+++ blockstile.ts
@@ -1,5 +1,5 @@
 //% weight=1000 color="#442255" icon="\uf45c"
-//% groups='["Tiles", "Events", "Logic"]'
+//% groups='["Tiles", "Events", "Conditions", "Actions"]'
 //% blockGap=8
 namespace TileWorld {
 
@@ -13,18 +13,36 @@ namespace TileWorld {
     export function setTileMap(map: Image) { 
         myWorld.setMap(map) 
     }
+    /**
+     * Set the background tile for sprites
+     * @param color
+     */
+    //% group="Tiles"
+    //% blockId=TWsetbackgroundtile block="set background tile to %color=colorindexpicker"
     export function setBackgroundTile(code: number) { 
         myWorld.setBackgroundTile(code)
     }
-    export function setTileCode(ts: TileSprite, code: number) { 
-        myWorld.setCode(ts, code)
-    }
-    export function addTile(code: number, image: Image) { 
+    /**
+     * Set an image as a tile at the given index. Tiles should be a 16x16 image
+     * @param index
+     * @param img
+     */
+    //% blockId=TWsettile block="set tile %index=colorindexpicker to %img=tile_image_picker"
+    //% group="Tiles"
+    export function setTile(code: number, image: Image) { 
         myWorld.addTiles(code, image)
     }
+    /**
+     * Set an image as a sprite at the given index. Sprites should be a 16x16 image
+     * @param index
+     * @param img
+     */
+    //% blockId=TWsetsprite block="set sprite %index=colorindexpicker to %img=tile_image_picker"
+    //% group="Tiles"
     export function addTileSprite(code: number, image: Image) { 
         myWorld.addTileSprites(code, image)
     }
+    
     export function getTileSprite(code: number) { 
         return myWorld.getSprite(code)
     }
@@ -33,11 +51,18 @@ namespace TileWorld {
     }
     
     // notifications
+    //% group="Events"
     export function onTileStationary(code: number, h: (ts: TileSprite) => void) { }
     export function onTileArrived(code: number, h: (ts: TileSprite, d: Dir) => void) { }
     export function onTileTransition(code: number, h: (ts: TileSprite, col: number, row: number) => void) { }
-    // logic
+    
+    // checks
     export function isOneOf(d: Dir, c1: Dir, c2: Dir = 0xff, c3: Dir = 0xff) { }
     export function isNotOneOf(d: Dir, c1: Dir, c2: Dir = 0xff, c3: Dir = 0xff) { }
+
+    // actions
+    export function setTileCode(ts: TileSprite, code: number) {
+        myWorld.setCode(ts, code)
+    }
 
 }
